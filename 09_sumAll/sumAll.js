d62e1079a751a977f032168fedc3a17267dfb38c
saido <38206432+JSaii@users.noreply.github.com>
2025-06-17 15:56:24 +0900

    Finish assignment 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+function checkInput(n){
+    return n >= 0 && n % 1 === 0 && typeof(n) === "number"
+}
 
+const sumAll = function(a, b) {
+    if(!checkInput(a) || !checkInput(b)){return "ERROR"};
+    const [firstNum, secondNum] = a < b ? [a,b] : [b,a]
+
+    let sum = 0;
+    for(i = firstNum;i<=secondNum;i++){
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
