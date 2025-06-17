5c0ce4538aa3f0201b66df62dc0a039f8975baf9
saido <38206432+JSaii@users.noreply.github.com>
2025-06-17 15:28:41 +0900

    Finish assignment 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+  const remove = Array.from(elements);
+  const newArray = arr.filter(item => !remove.includes(item))
+  return newArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
