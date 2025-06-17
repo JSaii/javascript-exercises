513cea250febeada5570a0370272163333951bbd
saido <38206432+JSaii@users.noreply.github.com>
2025-06-17 15:02:57 +0900

    Finish assignment 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const split = string.split('');
+    const reversed = [];
+    for(let i = split.length-1;i >= 0;i--){
+        reversed.push(split[i]);
+    }
+    const newString = reversed.join('');
+    return newString;
 };
 
 // Do not edit below this line
