471212ff7ef8ebcfc2a168a852da83c334567134
saido <38206432+JSaii@users.noreply.github.com>
2025-08-17 19:09:04 +0900

    finish

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const oldest = arr.reduce((acc, cur) => {
+        if(!("yearOfDeath" in cur)){
+            cur.age = currentYear - cur.yearOfBirth;
+        } else {
+            cur.age = cur.yearOfDeath - cur.yearOfBirth;
+        }
+        
+        if(cur.age > acc.age){
+            return cur;
+        } else {
+            return acc;
+        }
+    }, {age:0})
+    return oldest;
 };
 
 // Do not edit below this line
