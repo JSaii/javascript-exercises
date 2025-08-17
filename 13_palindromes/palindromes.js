9507d52e68a5f3cc645d4fcb035c68bcf23a7c13
saido <38206432+JSaii@users.noreply.github.com>
2025-08-17 14:48:19 +0900

    finish

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const clean = str.toLowerCase().replace(/[.!,\s]/g, '');
+    const array = clean.split('');
+    const reverse = array.reverse();
+    const join = reverse.join('');
 
+    if (join === clean){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
